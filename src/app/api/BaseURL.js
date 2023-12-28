730da96fdc03e24d99dac7ce12cf61a62be4caff
saadazghour <saad.azghour@gmail.com>
2023-12-28 12:39:47 +0100

    Update baseUrl

--- src/app/api/BaseURL.js
+++ src/app/api/BaseURL.js
@@ -1,2 +1,1 @@
-// export const baseUrl = 'http://ec2-16-170-143-20.eu-north-1.compute.amazonaws.com'
-export const baseUrl = 'http://ec2-16-170-143-20.eu-north-1.compute.amazonaws.com'
+export const baseUrl = import.meta.env.VITE_BASE_URL;
