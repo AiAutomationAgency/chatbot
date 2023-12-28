75b51ed0017a3dca8ab50419ff99526c36660af2
saadazghour <saad.azghour@gmail.com>
2023-12-28 12:28:59 +0100

    feat: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 // vite.config.js
 
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+
+// import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   esbuild: {
     loader: "jsx",
+  },
+
+  build: {
+    outDir: "build",
   },
 
   optimizeDeps: {
@@ -20,7 +27,21 @@ export default defineConfig({
   // depending on your application, base can also be "/"
   base: "",
 
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+
+    // { enforce: "pre", ...mdx() },
+    // react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+
+    // svgrPlugin({
+    //   svgrOptions: {
+    //     icon: true,
+    //     // ...svgr options (https://react-svgr.com/docs/options/)
+    //   },
+    // }),
+  ],
+
   server: {
     // this ensures that the browser opens upon server start
     open: true,
