c8d3d2cfc6d19fd5d0a5b1fd19a93cf4d9832d2b
saadazghour <saad.azghour@gmail.com>
2024-01-01 14:41:32 +0100

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@
 
 import { defineConfig } from "vite";
 
-// import react from "@vitejs/plugin-react";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react-swc";
 
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
